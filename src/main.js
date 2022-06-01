b2b62ab539260075aa519584001793403b499bc8
jolylai <jolylai163@163.com>
2022-06-01 17:33:03 +0800

    feat: vue-loader

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-console.log("main");
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.mount("#app");
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,31 @@
+const { DefinePlugin } = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
   mode: "development",
   entry: "/src/main.js",
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node-modules/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /.vue$/,
+        use: ["vue-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+    new VueLoaderPlugin(),
+    new DefinePlugin({
+      __VUE_OPTIONS_API__: "true",
+      __VUE_PROD_DEVTOOLS__: "false",
+    }),
+  ],
 };
